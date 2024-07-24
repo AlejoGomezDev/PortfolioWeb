9b70d7c047184914d48cd9602351497e41258682
KaliWick <168383538+KaliWick@users.noreply.github.com>
2024-07-24 16:48:46 -0300

    Agrego de carpetas esenciales

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//parsear json y urlencode
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Uso de archivos estaticos
+app.use(express.static(path.join(__dirname, '../public')));
+
+//ruta para el menu principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'html', 'index.html'));
+});
+
+//Manejo de rutas no encontradas
+app.use((req, res, next) => {
+    res.status(404).send('404 Not Found');
+});
+
+//Puerto
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
