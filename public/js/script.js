04ca2da8efdc954ddfa21cfd25e8cb706076bfd4
KaliWick <168383538+KaliWick@users.noreply.github.com>
2024-07-29 19:10:44 -0300

    v1.2
    Modificacion de proyectos y habilidades

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,45 @@
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-  
-      const target = document.querySelector(this.getAttribute('href'));
-      const navbarHeight = document.querySelector('.nav').offsetHeight;
-      const offsetTop = target.offsetTop - navbarHeight;
-  
-      window.scrollTo({
-        top: offsetTop,
-        behavior: 'smooth'
-      });
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    const navbarHeight = document.querySelector('.nav').offsetHeight;
+    const offsetTop = target.offsetTop - navbarHeight;
+
+    window.scrollTo({
+      top: offsetTop,
+      behavior: 'smooth'
     });
   });
+});
 const flipCards = document.querySelectorAll('.flipCard');
-    flipCards.forEach(card => {
-        card.addEventListener('click', function() {
-            this.classList.toggle('flip');
-        });
-    });
+flipCards.forEach(card => {
+  card.addEventListener('click', function () {
+    this.classList.toggle('flip');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const lenguajesButton = document.querySelector('.languageBTN');
+  const herramientasButton = document.querySelector('.ToolsBTN');
+  const lenguajesDiv = document.querySelector('.lenguajes');
+  const herramientasDiv = document.querySelector('.herramientas');
+  const buttons = document.querySelectorAll('.buttons button');
+
+  lenguajesDiv.style.display = 'grid';
+  herramientasDiv.style.display = 'none';
+
+  lenguajesButton.addEventListener('click', () => {
+    lenguajesDiv.style.display = 'grid';
+    herramientasDiv.style.display = 'none';
+    buttons.forEach(button => button.classList.remove('active'));
+    lenguajesButton.classList.add('active');
+  });
+
+  herramientasButton.addEventListener('click', () => {
+    lenguajesDiv.style.display = 'none';
+    herramientasDiv.style.display = 'grid';
+    buttons.forEach(button => button.classList.remove('active'));
+    herramientasButton.classList.add('active');
+  });
+});
