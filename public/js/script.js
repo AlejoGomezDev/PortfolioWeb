932609063a5b4c4f9704c2f61ec4a689c2b3fa77
KaliWick <168383538+KaliWick@users.noreply.github.com>
2024-07-28 21:35:52 -0300

    v1.1
    Nav
    Main
    Projects

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,20 @@
-
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      const target = document.querySelector(this.getAttribute('href'));
+      const navbarHeight = document.querySelector('.nav').offsetHeight;
+      const offsetTop = target.offsetTop - navbarHeight;
+  
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth'
+      });
+    });
+  });
+const flipCards = document.querySelectorAll('.flipCard');
+    flipCards.forEach(card => {
+        card.addEventListener('click', function() {
+            this.classList.toggle('flip');
+        });
+    });
