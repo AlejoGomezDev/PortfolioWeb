1ed777bd57117a3d345fd586361893596a4042d5
KaliWick <168383538+KaliWick@users.noreply.github.com>
2024-09-17 23:56:09 -0300

    Update menu-hamburguesa.js

--- public/js/menu-hamburguesa.js
+++ public/js/menu-hamburguesa.js
@@ -14,10 +14,10 @@
             d.querySelector(panel).classList.toggle("is-active");
             d.querySelector(panelBtn).classList.toggle("is-active");
         }
-        if(e.target.matches(menuLink)) {
+        if(e.target.matches(menuLink)){
             d.querySelector(panel).classList.remove("is-active");
             d.querySelector(panelBtn).classList.remove("is-active");
         }
-    }
+    };
     
  }
