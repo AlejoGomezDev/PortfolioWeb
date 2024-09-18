1ef415a645beb43b98d80e8c09894389793ec522
KaliWick <168383538+KaliWick@users.noreply.github.com>
2024-09-18 16:40:19 -0300

    v3.5.6

--- public/js/menu-hamburguesa.js
+++ public/js/menu-hamburguesa.js
@@ -1,23 +1,16 @@
- export default function hamburguerMenu(panelBtn, panel, menuLink){
+export default function hamburguerMenu(panelBtn, panel, menuLink) {
     const d = document;
-    d.addEventListener("click", (e) => {
-        handleMenuEvent(e);
-    });
-    
+  
     d.addEventListener("touchstart", (e) => {
-        handleMenuEvent(e);
-        console.log("touchstart event fired");
+      if (e.target.matches(panelBtn) || e.target.closest(panelBtn)) {
+        d.querySelector(panel).classList.toggle("is-active");
+        d.querySelector(panelBtn).classList.toggle("is-active");
+      }
+      if (e.target.matches(menuLink)) {
+        console.log(e.target);
+        d.querySelector(panel).classList.remove("is-active");
+        d.querySelector(panelBtn).classList.remove("is-active");
+      }
     });
-    
-    function handleMenuEvent(e) {
-        if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
-            d.querySelector(panel).classList.toggle("is-active");
-            d.querySelector(panelBtn).classList.toggle("is-active");
-        }
-        if(e.target.matches(menuLink)){
-            d.querySelector(panel).classList.remove("is-active");
-            d.querySelector(panelBtn).classList.remove("is-active");
-        }
-    };
-    
- }
+  }
+  
