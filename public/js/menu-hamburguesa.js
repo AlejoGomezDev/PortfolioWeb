3af87a24a9c38242f039bc1ed4fe289dcc1dd648
KaliWick <168383538+KaliWick@users.noreply.github.com>
2024-09-17 23:35:47 -0300

    v3.5.2
    cambio de click a touchstar para celulares

--- public/js/menu-hamburguesa.js
+++ public/js/menu-hamburguesa.js
@@ -1,14 +1,22 @@
  export default function hamburguerMenu(panelBtn, panel, menuLink){
     const d = document;
-    d.addEventListener("click",(e)=>{
-        if(e.target.matches(panelBtn)||e.target.matches(`${panelBtn} *`)){
+    d.addEventListener("click", (e) => {
+        handleMenuEvent(e);
+    });
+    
+    d.addEventListener("touchstart", (e) => {
+        handleMenuEvent(e);
+    });
+    
+    function handleMenuEvent(e) {
+        if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)) {
             d.querySelector(panel).classList.toggle("is-active");
             d.querySelector(panelBtn).classList.toggle("is-active");
         }
-        if(e.target.matches(menuLink)){
+        if(e.target.matches(menuLink)) {
             d.querySelector(panel).classList.remove("is-active");
             d.querySelector(panelBtn).classList.remove("is-active");
         }
-    });
+    }
     
  }
