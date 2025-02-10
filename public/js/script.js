36e4c0c8d038fd079b57654ef220a5556e79b8e4
KaliWick <168383538+KaliWick@users.noreply.github.com>
2025-02-10 20:22:22 -0300

    v5.0

--- public/js/script.js
+++ public/js/script.js
@@ -43,3 +43,74 @@ document.addEventListener("DOMContentLoaded", function() {
   var computedWidth = nameElement.scrollWidth + "px";
   nameElement.style.width = computedWidth;
 });
+
+document.addEventListener("keyup",(e)=>{
+  if(e.target.matches(".contact-form [required]")){
+    let $input = e.target,
+    pattern = $input.pattern;
+
+    if(pattern && $input.value !== ""){
+      let regex = new RegExp(pattern);
+      if(!regex.exec($input.value))return;
+    }
+  }
+})
+var $contactForm = document.getElementById("contact-form");
+var $input = document.querySelectorAll("#contact-form [required]");
+var $formError = document.querySelector(".contact-form-error");
+var $formResponse = document.querySelector(".contact-form-response");
+var $submitBtn = document.querySelector("#submit");
+
+function validarForm() {
+  let isValid = true;
+  $input.forEach(($input) => {
+    let pattern = $input.pattern;
+    let value = $input.value;
+
+    if (pattern && value !== "") {
+      let regex = new RegExp(pattern);
+      if (!regex.test(value)) {
+        isValid = false;
+      }
+    }
+  });
+
+  return isValid;
+}
+document.addEventListener("submit",(e)=>{
+  e.preventDefault();
+
+  if(!validarForm()){
+    $formError.classList.remove("none");
+    $formResponse.classList.add("none");
+    return;
+  }
+  else{
+    $formError.classList.add("none");
+    $submitBtn.value = "Enviando...";
+    $submitBtn.classList.add("disabled");
+  }
+  fetch("https://formsubmit.co/ajax/alejongomez@gmail.com",{
+    method:"POST",
+    body: new FormData(e.target)
+  })
+  .then(res=> res.ok?res.json(): Promise.reject(res))
+  .then(json=>{
+    $formResponse.classList.remove("none");
+    $contactForm.reset();
+    $submitBtn.classList.remove("disabled");
+    $submitBtn.value = "Enviar";
+    setTimeout(() => {
+      $formResponse.classList.add("none");
+    }, 2000);
+  })
+  .catch(err=>{
+    console.log(err);
+    $submitBtn.disabled = false;
+    $submitBtn.value = "Enviar";
+    $submitBtn.classList.remove("disabled");
+  })
+
+  
+  
+})
